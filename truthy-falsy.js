15f1b14c00852aaec11744e243106c4be8df97e6
THIAGO BOGONI SPINDOLA <thiago_spdvr@hotmail.com>
2022-11-25 19:36:19 -0300

    Adição de novos arquivos e códigos

---
+++ truthy-falsy.js
@@ -0,0 +1,10 @@
+// console.log('Esse resultado é ', 1 == false)
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = 'alura';
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
